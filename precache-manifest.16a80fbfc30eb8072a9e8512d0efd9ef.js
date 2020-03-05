c6d21d26ed1459b7ffe94fee56ee03d542edf3e9
sheen4n <sheen.an.goh@gmail.com>
2020-03-05 14:31:34 +0800

    Updates

---
+++ precache-manifest.16a80fbfc30eb8072a9e8512d0efd9ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1521d9f4d15f88b6498b97d4034671b7",
+    "url": "/react-pic-search/index.html"
+  },
+  {
+    "revision": "31fcca7443dfa77c2b0a",
+    "url": "/react-pic-search/static/css/main.c00f04db.chunk.css"
+  },
+  {
+    "revision": "229922ea3dce321b1fb7",
+    "url": "/react-pic-search/static/js/2.09f97919.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-pic-search/static/js/2.09f97919.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31fcca7443dfa77c2b0a",
+    "url": "/react-pic-search/static/js/main.8ac78e5c.chunk.js"
+  },
+  {
+    "revision": "b56a9250ec1eb5145c76",
+    "url": "/react-pic-search/static/js/runtime-main.750bf53e.js"
+  }
+]);
